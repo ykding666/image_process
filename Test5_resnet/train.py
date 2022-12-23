f4e4b949fa0059ab87825cff138ef0065320278a
ykding666 <1103720943@qq.com>
2022-12-23 15:27:37 +0800

    train2

--- Test5_resnet/train.py
+++ Test5_resnet/train.py
@@ -80,7 +80,7 @@ def main():
     params = [p for p in net.parameters() if p.requires_grad]
     optimizer = optim.Adam(params, lr=0.0001)
 
-    epochs = 18
+    epochs = 16
     best_acc = 0.0
     save_path = './resNet34.pth'
     train_steps = len(train_loader)
